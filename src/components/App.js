52e8784358607ff33722f855addd99fab56ce799
“Drmingler” <kayymedia@gmail.com>
2020-06-09 20:36:19 +0200

    feat : Add  Header component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainNavBar from "./MainNavBar";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavBar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
