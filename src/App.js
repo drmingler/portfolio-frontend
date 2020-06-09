5751c24e796f890c17ee5fb072b7feaf95bf74cf
“Drmingler” <kayymedia@gmail.com>
2020-06-09 18:29:17 +0200

    feat : Add all necessary API calls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getUser, getAchievement, getProject, getAchievements, getProjects, getUsers} from "./utils/api";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+  useEffect(() => {
+    getUsers().then(data => console.log(data));
+  }, []);
+  return <div>"Hello"</div>;
 }
 
 export default App;
