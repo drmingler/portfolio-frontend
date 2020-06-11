3825a82e1317d38b5ec2e684e163732cdfa282b9
“Drmingler” <kayymedia@gmail.com>
2020-06-11 05:21:28 +0200

    feat : Add Project component

---
+++ src/components/Project.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+
+const Project = ({ eachProject }) => {
+  const { name, picture, stacks, description, siteLink } = eachProject;
+  return (
+    <Fragment>
+      <div className="project-img">
+        <img src={picture} alt="project-img" />
+      </div>
+      <div className="project-details">
+        <div className="project-card__info">
+          <h3 className="project-card__title">{name}</h3>
+          <p className="project-card__description">{description}</p>
+          <p className="project-card__stack">Used stack:</p>
+          <ul className="list_of_stacks">
+            {stacks.map((stack, index) => (
+              <li key={index}>{stack}</li>
+            ))}
+          </ul>
+          <span className="project-card__link">
+            {/*<button className={"source-btn"}>*/}
+            {/*  View Source*/}
+            {/*</button>*/}
+            {siteLink}
+          </span>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Project;
