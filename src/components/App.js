a38a42dd26bc1629232b5ec80f6f161f16c1734d
“Drmingler” <kayymedia@gmail.com>
2020-06-12 07:36:59 +0200

    fix : Loading page not showing up

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MainNavBar from "./MainNavBar";
 import Header from "./Header";
 import ResumeSection from "./ResumeSection";
 import Footer from "./Footer";
 import ProjectSection from "./ProjectsSection";
+import { useContact } from "../hooks/hooks";
+import { Loader } from "semantic-ui-react";
 
 function App() {
+  const user = useContact();
   return (
     <div className="App">
-      <MainNavBar />
-      <Header />
-      <ResumeSection />
-      <ProjectSection />
-      <Footer />
+      {user.id ? (
+        <Fragment>
+          <MainNavBar />
+          <Header user={user} />
+          <ResumeSection />
+          <ProjectSection />
+          <Footer />
+        </Fragment>
+      ) : (
+        <div className="loader">
+          <Loader active inline="centered" />
+        </div>
+      )}
     </div>
   );
 }
