796034a16b42be80b235796f1a624b3528511477
“Drmingler” <kayymedia@gmail.com>
2020-06-13 21:56:51 +0200

    fix : ContactForm not sending  post request to the API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,55 +1,53 @@
-const URL = "http://localhost:8000";
+const URL = "http://127.0.0.1:8000/api";
 
 const headers = {
   Accept: "application/json"
 };
 
 export const getUsers = () =>
-  fetch(`${URL}/users`, {
+  fetch(`${URL}/users/`, {
     method: "GET",
     headers
   }).then(response => response.json());
 
-export const getUser = (id) =>(
-    fetch(`${URL}/users/${id}`, {
-        method : "GET",
-        headers
-    }).then(response=>response.json())
-);
+export const getUser = id =>
+  fetch(`${URL}/users/${id}`, {
+    method: "GET",
+    headers
+  }).then(response => response.json());
 
-export const getProjects = ()=>(
-    fetch(`${URL}/projects/`, {
-        method : "GET",
-        headers
-    }).then(response=>response.json())
-    );
+export const getProjects = () =>
+  fetch(`${URL}/projects/`, {
+    method: "GET",
+    headers
+  }).then(response => response.json());
 
-export const getProject = (id)=>(
-    fetch(`${URL}/projects/${id}`, {
-        method : "GET",
-        headers
-    }).then(response=>response.json())
-);
+export const getProject = id =>
+  fetch(`${URL}/projects/${id}`, {
+    method: "GET",
+    headers
+  }).then(response => response.json());
 
+export const getAchievements = () =>
+  fetch(`${URL}/achievements/`, {
+    method: "GET",
+    headers
+  }).then(response => response.json());
 
-export const getAchievements = ()=>(
-    fetch(`${URL}/achievements/`, {
-        method : "GET",
-        headers
-    }).then(response=>response.json())
-);
+export const getAchievement = id =>
+  fetch(`${URL}/achievements/${id}`, {
+    method: "GET",
+    headers
+  }).then(response => response.json());
 
-export const getAchievement = (id)=>(
-    fetch(`${URL}/achievements/${id}`, {
-        method : "GET",
-        headers
-    }).then(response=>response.json())
-);
-
-export const contactForm = (data)=>(
-    fetch(`${URL}/contact/`, {
-        method : "POST",
-        headers,
-        body : JSON.stringify(data)
-    }).then(response=>response.json())
-);
+export const contactForm = data =>
+  fetch(`${URL}/contact/`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .catch(response => response);
